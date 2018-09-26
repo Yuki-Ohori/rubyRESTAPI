e5d9623a8f62e2adadc8f50c80c6aa158a34c3e9
Ohori Yuki <yuki.ohori@vrg.jp>
2018-09-26 19:28:38 +0900

    header added

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,12 @@ module RubyRESTAPI
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
