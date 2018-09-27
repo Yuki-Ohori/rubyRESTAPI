c6cf694e8192c8df5086333da0773f12e8712ec3
Ohori Yuki <yuki.ohori@vrg.jp>
2018-09-27 09:47:29 +0900

    api config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,6 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Per-Page', 'Total', 'Link']
+      expose: ['Per-Page', 'Total', 'Link'],
+      max_age: 600
   end
 end
